0f9fab2acfabeb2d22e2d49f311005c706695f5e
K_YoonHan <reprica123@gmail.com>
2023-06-27 15:58:33 +0900

    uploaded at 2023. 6. 27. 오후 3:58:34

---
+++ programmers/문자열 붙여서 출력하기/solution.cpp
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181946
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str1, str2;
+    cin >> str1 >> str2;
+    
+    cout << str1 << str2;
+    
+    return 0;
+}
