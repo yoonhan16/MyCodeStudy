14541c491afd37b7e8db5c445e9b1a9d491252d7
K_YoonHan <reprica123@gmail.com>
2023-06-27 15:58:28 +0900

    uploaded at 2023. 6. 27. 오후 3:58:29

---
+++ programmers/덧셈식 출력하기/solution.cpp
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181947
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << a  << ' ' << '+' << ' ' << b << ' ' << '=' << ' ' << a + b << endl;
+    return 0;
+}
