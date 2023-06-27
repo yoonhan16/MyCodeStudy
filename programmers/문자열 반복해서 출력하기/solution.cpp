ab0c58382de879f752cd089a741f6b3d966e3bbe
K_YoonHan <reprica123@gmail.com>
2023-06-27 15:57:50 +0900

    uploaded at 2023. 6. 27. 오후 3:57:50

---
+++ programmers/문자열 반복해서 출력하기/solution.cpp
@@ -0,0 +1,19 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181950
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    string str;
+    int n;
+    cin >> str >> n;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cout << str;
+    }
+    
+    return 0;
+}
