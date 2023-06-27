b506d1cda61e0bff9693b4a9e07190d695923627
K_YoonHan <reprica123@gmail.com>
2023-06-27 15:57:39 +0900

    uploaded at 2023. 6. 27. 오후 3:57:40

---
+++ programmers/a와 b 출력하기/solution.cpp
@@ -0,0 +1,14 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181951
+
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int a;
+    int b;
+    cin >> a >> b;
+    cout << "a = " << a << endl; 
+    cout << "b = " << b << endl;
+    return 0;
+}
