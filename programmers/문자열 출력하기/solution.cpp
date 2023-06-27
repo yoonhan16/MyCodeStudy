f3994e77fbcb97c0d24ae0e0685f3e37a11ed77e
K_YoonHan <reprica123@gmail.com>
2023-06-27 15:56:20 +0900

    uploaded at 2023. 6. 27. 오후 3:56:20

---
+++ programmers/문자열 출력하기/solution.cpp
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181952
+
+#pragma GCC("Ofast")
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    string str;
+    getline(cin, str);
+    cout << str << endl;
+    return 0;
+}
