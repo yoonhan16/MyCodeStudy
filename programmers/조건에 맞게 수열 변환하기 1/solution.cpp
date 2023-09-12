d98c35fbbee015e1c3e8afd9a2ddd5f5d6732d94
K_YoonHan <reprica123@gmail.com>
2023-09-12 19:26:08 +0900

    uploaded at 2023. 9. 12. 오후 7:26:07

---
+++ programmers/조건에 맞게 수열 변환하기 1/solution.cpp
@@ -0,0 +1,29 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181882
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+    
+    for (int i = 0; i < arr.size(); i++)
+    {
+        if (arr[i] % 2 == 1 && arr[i] < 50)
+        {
+            answer.push_back(arr[i] * 2);
+        }
+        else if (arr[i] % 2 == 0 && arr[i] >= 50)
+        {
+            answer.push_back(arr[i] / 2);
+        }
+        else
+        {
+            answer.push_back(arr[i]);
+        }
+    }
+    
+    return answer;
+}
