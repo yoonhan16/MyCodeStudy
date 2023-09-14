398485023992d930f16f8a2b3442d9ca18906ef8
K_YoonHan <reprica123@gmail.com>
2023-09-14 20:16:53 +0900

    uploaded at 2023. 9. 14. 오후 8:16:52

---
+++ programmers/문자열로 변환/solution.cpp
@@ -0,0 +1,13 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181845
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(int n) 
+{
+    string answer = to_string(n);
+    
+    return answer;
+}
