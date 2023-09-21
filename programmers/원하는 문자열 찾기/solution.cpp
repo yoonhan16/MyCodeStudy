f7a33fb790b229cc148a8aca388721e52df5b95e
K_YoonHan <reprica123@gmail.com>
2023-09-21 23:06:18 +0900

    uploaded at 2023. 9. 21. 오후 11:06:18

---
+++ programmers/원하는 문자열 찾기/solution.cpp
@@ -0,0 +1,16 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/181878
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(string word, string pat) 
+{
+    
+    transform(word.begin(), word.end(), word.begin(), ::toupper);
+    transform(pat.begin(), pat.end(), pat.begin(), :: toupper);
+    
+    return word.find(pat) != string::npos;
+}
