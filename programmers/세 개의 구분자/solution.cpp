2eef6c6257778ca91e7b70e3f87b502fa6759b8c
K_YoonHan <reprica123@gmail.com>
2023-10-18 14:10:23 +0900

    updated at 2023. 10. 18. 오후 2:10:23

--- programmers/세 개의 구분자/solution.cpp
+++ programmers/세 개의 구분자/solution.cpp
@@ -8,56 +8,56 @@ using namespace std;
 vector<string> solution(string myStr) 
 {
     vector<string> answer;
-    // string temp;
-    // for (int idx = 0; idx < myStr.size(); idx++)
-    // {
-    //     if (myStr[idx] > 'c')
-    //     {
-    //         temp += myStr[idx];
-    //     }
-    //     else
-    //     {
-    //         if (temp.empty())
-    //         {
-    //             continue;
-    //         }
-    //         else
-    //         {
-    //             answer.push_back(temp);
-    //             temp = "";
-    //         }
-    //     }
-    // }
-    // if (temp.empty())
-    // {
-    //     answer.push_back("EMPTY");
-    // }
-    // else
-    // {
-    //     answer.push_back(temp);
-    // }
-    
-    for (int i = 0; i < myStr.size(); i++)
+    string temp;
+    for (int idx = 0; idx < myStr.size(); idx++)
     {
-        if (myStr[i] == 'a' || myStr[i] == 'b' || myStr[i] == 'c')
+        if (myStr[idx] > 'c')
         {
-            myStr[i] = ' ';
+            temp += myStr[idx];
+        }
+        else
+        {
+            if (temp.empty())
+            {
+                continue;
+            }
+            else
+            {
+                answer.emplace_back(temp);
+                temp = "";
+            }
         }
     }
-    
-    string str_temp;
-    stringstream ss;
-    ss.str(myStr);
-    
-    while (ss >> str_temp)
+    if (temp.empty())
     {
-        answer.push_back(str_temp);
+        answer.emplace_back("EMPTY");
+    }
+    else
+    {
+        answer.emplace_back(temp);
     }
     
-    if (answer.empty())
-    {
-        answer.push_back("EMPTY");
-    }
+//     for (int i = 0; i < myStr.size(); i++)
+//     {
+//         if (myStr[i] == 'a' || myStr[i] == 'b' || myStr[i] == 'c')
+//         {
+//             myStr[i] = ' ';
+//         }
+//     }
+    
+//     string str_temp;
+//     stringstream ss;
+//     ss.str(myStr);
+//    
+//     while (ss >> str_temp)
+//     {
+//         answer.emplace_back(str_temp);
+//     }
+//    
+//     if (answer.empty())
+//     {
+//         answer.emplace_back("EMPTY");
+//     }
 
     return answer;
 }
