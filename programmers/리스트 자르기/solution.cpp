6802915b5423c296830dc23b489c2e4f43e3c248
K_YoonHan <reprica123@gmail.com>
2023-10-24 15:48:20 +0900

    updated at 2023. 10. 24. 오후 3:48:20

--- programmers/리스트 자르기/solution.cpp
+++ programmers/리스트 자르기/solution.cpp
@@ -45,3 +45,16 @@ vector<int> solution(int n, vector<int> slicer, vector<int> num_list)
     
     return answer;
 }
+
+    // switch(n)
+    // {
+    //     case 1: return vector<int>(num_list.begin(), num_list.begin()+b+1);
+    //     case 2: return vector<int>(num_list.begin()+a, num_list.end());
+    //     case 3: return vector<int>(num_list.begin()+a, num_list.begin()+b+1);
+    //     case 4: {
+    //         vector<int> answer;
+    //         for(int i = a; i <= b; i += c)
+    //             answer.push_back(num_list[i]);
+    //         return answer;
+    //     }
+    // }
